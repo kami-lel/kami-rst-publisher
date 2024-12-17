17a88ea50090cc0801221f3d6e4f9c34f25ac659
kami lel <77594253+kami-lel@users.noreply.github.com>
2024-12-16 16:08:27 -0800

    detect issues with pubilsh_string

--- kami_rst_publisher/cli_web_server.py
+++ kami_rst_publisher/cli_web_server.py
@@ -9,7 +9,7 @@ WEB_SERVER_DEFAULT_PORT = 8080
 from http.server import HTTPServer, BaseHTTPRequestHandler
 
 from docutils import io
-from docutils.core import publish_programmatically
+from docutils.core import publish_programmatically, publish_string
 
 from .cli_utils import determine_parser, create_settings_overrides
 
@@ -17,6 +17,16 @@ from .cli_utils import determine_parser, create_settings_overrides
 def generate_html_content_on_http_request():
     global src_file_path
     global render_preset
+
+    # HACK
+    with open('tests/test_test.rst', 'r') as f:
+        ipt = f.read()
+        opt = publish_string(ipt)
+
+        print('test')
+        return opt
+
+
 
     # BUG can not properly publish
     output, publisher = publish_programmatically(
@@ -28,7 +38,7 @@ def generate_html_content_on_http_request():
         writer=None, writer_name='html5',
         settings=None, settings_spec=None,
                 settings_overrides=create_settings_overrides(render_preset),
-        config_section=None, enable_exit_status=None)
+        config_section=None, enable_exit_status=False)
 
     return output
 
@@ -41,6 +51,11 @@ class CustomHTTPRequestHandler(BaseHTTPRequestHandler):
         self.end_headers()
 
         html_content = generate_html_content_on_http_request()
+
+        # HACK
+        with open('output.html', 'r') as f:
+            html_content = f.read().encode('utf-8')
+
         self.wfile.write(html_content)
 
 
